fa3a78ad09fdef4d674399a438c2736a2be0480b
Arfaan <90722310+mdarfaanbaig@users.noreply.github.com>
2024-04-02 15:46:21 +0530

    Update __init.py

--- src/csv2database/__init.py
+++ src/csv2database/__init.py
@@ -1,4 +1,4 @@
-from .csv_import import GoogleDriveImporter, LocalCSVImporter
+from .csv_import import GoogleDriveImporter, LocalCSVImporter, GoogleSheetImporter
 
 # List of classes to be exposed when the package is imported
 __all__ = ['GoogleDriveImporter', 'LocalCSVImporter','GoogleSheetImporter']
