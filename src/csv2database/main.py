ddd1ab4f39c630ea93d7f83adb10363b8a775131
Arfaan <90722310+mdarfaanbaig@users.noreply.github.com>
2024-04-02 15:15:15 +0530

    Update main.py

--- src/csv2database/main.py
+++ src/csv2database/main.py
@@ -1,6 +1,6 @@
 import os
 os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = r"D:\Beginest_Project\beginest-service-account-key.json"
-from csv2database.csv_import import LocalCSVImporter, GoogleDriveImporter
+from csv2database.csv_import import LocalCSVImporter, GoogleDriveImporter,GoogleSheetImporter
 
 def main():
 
@@ -13,12 +13,22 @@ def main():
         print("Error importing local CSV files.")
 
     # Call the load_csv_to_mysql function for Google Drive file
-    google_drive_file_id = "1RvpZBm6vWZE9V4V8TWwM26IZsdWnzOyg"
+    google_drive_file_id = "1meuL4jkluBZhrcYBriaIgLrFfwt5q-JO"
     success_google_drive = GoogleDriveImporter(google_drive_file_id,create_fresh=False).load_csv_to_mysql("localhost", "my_database", "root", "Arfaan@123456")
     if success_google_drive:
         print("Google Drive CSV file imported successfully.")
     else:
         print("Error importing Google Drive CSV file.")
+    
+    spreadsheet_id="1eTwI6jKzQiKrvaYUUIAluFTykFNFnNfY6Kf6ibhdjas"
+    sheet_name =  "WaterData"
+    success_google_sheet = GoogleSheetImporter(spreadsheet_id,sheet_name,create_fresh=False).load_sheet_to_mysql("localhost", "my_database", "root", "Arfaan@123456")
+    if success_google_sheet:
+        print("Data imported successfully from Google Sheet.")
+    else:
+        print("Failed to import data from Google Sheet.")
+
 
 if __name__ == "__main__":
     main()
+
